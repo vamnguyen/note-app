7558307aa90a25b6246734455351f852c3ed3f92
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-08-21 08:35:39 +0700

    update path server deployed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const GRAPHQL_SERVER = "http://localhost:4000";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000/graphql";
+export const GRAPHQL_SERVER = "https://note-app-u02w.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
+  "wss://note-app-u02w.onrender.com/graphql";
